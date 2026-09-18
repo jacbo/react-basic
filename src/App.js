f8a1de0179824b29e344d1538b0d340dda6d6406
songmengqiang <8280519+songmengqiang@user.noreply.gitee.com>
2026-09-18 15:56:14 +0800

    自定义hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState } from 'react'
+
+
+function useToggle(){
+    const [toggle,setToggle] = useState(false)
+
+    const handleToggle = () => { 
+        setToggle(!toggle)
+    }
+    return [toggle,handleToggle]
+}
 
 export default function App() { 
+    const [toggle,handleToggle] = useToggle()
+
     return (
         <div className="App"> 
-            this is app
+            {toggle && <div>this is div</div>}
+            <button onClick={handleToggle}>Toggle</button>
         </div>
     )
 }
